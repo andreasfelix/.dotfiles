e97c30628b385e394c43f271339ed2cd8a1ee3a1
felix <felix>
2019-04-08 03:22:08 +0200

    added v option to makelinks

--- makelinks.sh
+++ makelinks.sh
@@ -1,11 +1,11 @@
 dotfiles="$(cd $(dirname "$0"); pwd)"
 
-ln -sf $dotfiles/bash_aliases $HOME/.bash_aliases
-ln -sf $dotfiles/bash_custom $HOME/.bash_custom
-ln -sf $dotfiles/gitconfig $HOME/.gitconfig
-ln -sf $dotfiles/vimrc $HOME/.vimrc
-ln -sf $dotfiles/intellij/.ideavimrc $HOME/.ideavimrc
-ln -sf $dotfiles/vscode/settings.json $HOME/.var/app/com.visualstudio.code.oss/config/Code\ -\ OSS/User
-ln -sf $dotfiles/vscode/settings.json $HOME/.config/Code/User/settings.json
-ln -sf $dotfiles/inputrc $HOME/.inputrc
-ln -sf $dotfiles/pyrc $HOME/.pyrc
+ln -sfv $dotfiles/bash_aliases $HOME/.bash_aliases
+ln -sfv $dotfiles/bash_custom $HOME/.bash_custom
+ln -sfv $dotfiles/gitconfig $HOME/.gitconfig
+ln -sfv $dotfiles/vimrc $HOME/.vimrc
+ln -sfv $dotfiles/intellij/.ideavimrc $HOME/.ideavimrc
+ln -sfv $dotfiles/vscode/settings.json $HOME/.var/app/com.visualstudio.code.oss/config/Code\ -\ OSS/User
+ln -sfv $dotfiles/vscode/settings.json $HOME/.config/Code/User/settings.json
+ln -sfv $dotfiles/inputrc $HOME/.inputrc
+ln -sfv $dotfiles/pyrc $HOME/.pyrc
