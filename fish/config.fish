860256a6f498735a9c319f26c8fd93df4adbf616
felix <felix.andreas95@googlemail.com>
2019-07-03 18:59:26 +0200

    update

--- fish/config.fish
+++ fish/config.fish
@@ -7,19 +7,19 @@ function fish_greeting
 end
 
 function fish_prompt
-  set_color cyan --bold
+  set_color cyan
   printf "%s" "$USER"
   set_color normal
   printf " using fish at %s in " $hostname
-  set_color blue --bold
+  set_color blue
   printf "%s" "$PWD"
   set -l git_branch (git branch ^/dev/null | sed -n '/\* /s///p')
   if test -n "$git_branch"
     set_color normal
     printf " on "
-    set_color yellow --bold
+    set_color yellow
     printf "%s" "$git_branch"
   end
-  set_color red --bold
+  set_color red
   printf "\f\r\$ "
 end
