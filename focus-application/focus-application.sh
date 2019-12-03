83ecc5869c302ed556f6754085bd0aa526d99d88
felix <felix.andreas95@googlemail.com>
2019-12-03 01:07:47 +0100

    format focus-application.sh

--- focus-application/focus-application.sh
+++ focus-application/focus-application.sh
@@ -2,19 +2,25 @@
 #  ARG1: WM_CLASS of application
 #  ARG2: /path/to/application
 # Description: Use this script to create a keyboard shortcut to focus an application.
-#  Case 1: application is not running           --> open a new instance
-#  Case 2: application is running without focus --> focus instance with highest z-index
-#  Case 3: application is running and has focus --> focus another instance (lowest z-index)
+#  Case A: application is not running           --> open a new instance
+#  Case B: application is running without focus --> focus instance with highest z-index
+#  Case C: application is running and has focus --> focus another instance (lowest z-index)
 
-class_ids=$(xdotool search --onlyvisible --classname $1 | head -n "$(wmctrl -lx | awk '{ print $3 }' | grep $1 -c)")
-if [ -z "$class_ids" ] ; then # case 1
-    $2 &
+class_ids=$(xdotool search --onlyvisible --classname $1 \
+          | head -n "$(wmctrl -lx | awk '{ print $3 }' | grep $1 -c)")
+
+if [ -z "$class_ids" ] ; then
+    # Case A
+    $2 & disown
 else
     current_id=$(xdotool getactivewindow)
-    if [ -z "$(echo $class_ids | grep $current_id)" ]; then # case 2
+    if [ -z "$(echo $class_ids | grep $current_id)" ]; then
+        # Case B
         new_id=$(echo $class_ids | awk '{ print $NF}')
-    else # case 3
+    else
+        # Case C
         new_id=$(echo $class_ids | awk '{ print $1}')
     fi
+
     xdotool windowactivate "$new_id"
 fi
