d44769b3f7704c3cb0ed50045dfc11d0e9bf7421
felix <felix.andreas95@googlemail.com>
2019-12-03 04:12:38 +0100

    create .local/bin if not exists

--- makelinks.sh
+++ makelinks.sh
@@ -2,6 +2,7 @@ dotfiles=$(dirname $(readlink -f $0))
 
 ln -sfv $dotfiles/bash_aliases $HOME/.bash_aliases
 ln -sfv $dotfiles/bash_custom $HOME/.bash_custom
+mkdir -p $HOME/.local/bin
 ln -sfv $dotfiles/focus-application/focus-application.sh $HOME/.local/bin/focus-application
 ln -sfv $dotfiles/gitconfig $HOME/.gitconfig
 ln -sfv $dotfiles/ideavimrc $HOME/.ideavimrc
