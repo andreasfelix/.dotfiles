e7d179a806e833201eee729a62ddadbe4732440d
Felix Andreas <felix.andreas95@googlemail.com>
2020-10-16 14:48:22 +0200

    add config.nix to dotfiles

--- makelinks.sh
+++ makelinks.sh
@@ -2,12 +2,15 @@ dotfiles=$(dirname $(readlink -f $0))
 
 ln -sfv $dotfiles/bash_aliases $HOME/.bash_aliases
 ln -sfv $dotfiles/bash_custom $HOME/.bash_custom
-mkdir -p $HOME/.local/bin
-ln -sfv $dotfiles/focus-application/focus-application.sh $HOME/.local/bin/focus-application
+ln -sfv $dotfiles/config.nix $HOME/.config/nixpkgs
 ln -sfv $dotfiles/gitconfig $HOME/.gitconfig
 ln -sfv $dotfiles/ideavimrc $HOME/.ideavimrc
 ln -sfv $dotfiles/inputrc $HOME/.inputrc
 ln -sfv $dotfiles/vimrc $HOME/.vimrc
-ln -sfv $dotfiles/vscode-settings.json $HOME/.var/app/com.visualstudio.code.oss/config/Code\ -\ OSS/User
+ln -sfv $dotfiles/vscode-settings.json $HOME/.config/Code/User
 ln -sfv $dotfiles/vscode-settings.json $HOME/.var/app/com.visualstudio.code/config/Code/User
-ln -sfv $dotfiles/vscode-settings.json $HOME/.config/Code/User/settings.json
+ln -sfv $dotfiles/vscode-settings.json $HOME/.var/app/com.visualstudio.code.oss/config/Code\ -\ OSS/User
+
+mkdir -p $HOME/.local/bin
+ln -sfv $dotfiles/focus-application/focus-application.sh $HOME/.local/bin/focus-application
+
