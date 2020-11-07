6045dae9eb64b70716bd097d52e2c17557861bb5
Felix Andreas <felix.andreas95@googlemail.com>
2020-11-07 01:32:47 +0100

    update bash config

--- config.bash
+++ config.bash
@@ -5,21 +5,20 @@
 #     . ~/.bash_config
 # fi
 
+# bash options
 stty -ixon      # disable terminal scroll lock (Ctrl+S/Ctrl+q)
 shopt -s autocd # auto change directories
 
 # prompt: <user> at <host> in /path/to/cwd on <branch>
-source $HOME/.nix-profile/share/git/contrib/completion/git-prompt.sh
-export PS1='\e[36m\u \e[0mat \h in \e[34m$PWD $(__git_ps1 "\e[0mon \e[33m%s")\n '
+export PS1='\e[1;31m\u \e[0mat \e[1;34m\h \e[0min \e[1;36m$PWD $(__git_ps1 "\e[0mon \e[1;33m%s")\e[0m\n '
 export PS2='>'
 
-# enable pazi
-if command -v pazi &>/dev/null; then
-  eval "$(pazi init bash)"
+# enable zoxide
+if command -v zoxide &>/dev/null; then
+    eval "$(zoxide init bash)"
 fi
 
 # enable direnv
 if command -v direnv &>/dev/null; then
     eval "$(direnv hook bash)"
 fi
-
