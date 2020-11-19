c411eca6f87ac9e5cc01317e69564f98dc74acde
Felix Andreas <felix.andreas95@googlemail.com>
2020-11-19 03:24:14 +0100

    update bash config

--- aliases.bash
+++ aliases.bash
@@ -36,10 +36,10 @@ alias docker-remove-untagged-images='docker rmi $(docker images -q --filter "dan
 
 # misc
 alias activate-canon="gphoto2 --stdout --capture-movie | ffmpeg -i - -vf format=yuv420p -f v4l2 /dev/video0"
+alias activate-v4l2loopback="sudo modprobe v4l2loopback exclusive_caps=1 max_buffers=2"
 ch() { curl cheat.sh/$1; }
 alias dfs='df -hx"squashfs"' # dont show snaps
 alias record-gif='ffmpeg -video_size 1920x1080 -f x11grab -i :0.0+0,420 -y video.gif -vf "fps=10,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0'
-alias screenkey='unset XMODIFIERS ; unset GTK_IM_MODULES; unset QT_IM_MODULES; /usr/bin/screenkey'
 svg2png() { for file in *.svg; do inkscape -d=768 $file -e ${file%svg}png; done ; }
 watch-cpu() { watch -n 0.5 $'mpstat 1 1 | awk \'/^Average/ {print 100-$NF,"%"}\''; }
 
--- config.bash
+++ config.bash
@@ -10,7 +10,7 @@ stty -ixon      # disable terminal scroll lock (Ctrl+S/Ctrl+q)
 shopt -s autocd # auto change directories
 
 # prompt: <user> at <host> in /path/to/cwd on <branch>
-export PS1='\e[1;31m\u \e[0mat \e[1;34m\h \e[0min \e[1;36m$PWD $(__git_ps1 "\e[0mon \e[1;33m%s")\e[0m\n '
+export PS1='\e[1;31m\u \e[0mat \e[1;34m\h \e[0min \e[1;36m$PWD\e[0m$(__git_ps1 " on \e[1;33m%s\e[0m")\n '
 export PS2='>'
 
 # enable zoxide
