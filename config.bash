c1333bb65c377cad61228015da5e42eb3bae0549
Felix Andreas <felix.andreas95@googlemail.com>
2020-11-25 01:19:18 +0100

    do not export PS1

--- config.bash
+++ config.bash
@@ -10,8 +10,8 @@ stty -ixon      # disable terminal scroll lock (Ctrl+S/Ctrl+q)
 shopt -s autocd # auto change directories
 
 # prompt: <user> at <host> in /path/to/cwd on <branch>
-export PS1='\e[1;31m\u \e[0mat \e[1;34m\h \e[0min \e[1;36m$PWD\e[0m$(__git_ps1 " on \e[1;33m%s\e[0m")\n '
-export PS2='>'
+PS1='\033[1;31m\u \e[0mat \e[1;34m\h \e[0min \e[1;36m$PWD\e[0m$(__git_ps1 " on \e[1;33m%s\e[0m")\n '
+PS2='>'
 
 # enable zoxide
 if command -v zoxide &>/dev/null; then
