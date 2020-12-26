bf9b2a4282ace13d94a04f5c7d110933c4d8608f
Felix Andreas <felix.andreas95@googlemail.com>
2020-12-26 12:37:03 +0100

    update set-shortcuts.py

--- focus-application/set-shortcuts.py
+++ focus-application/set-shortcuts.py
@@ -9,9 +9,9 @@ paths = [
     f"/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom{i}/"
     for i in range(len(applications))
 ]
-subprocess.call(["/usr/bin/gsettings", "set", schema, "custom-keybindings", str(paths)])
+subprocess.call(["gsettings", "set", schema, "custom-keybindings", str(paths)])
 for path, (wm_class, app, key) in zip(paths, applications):
-    tmp = ["/usr/bin/gsettings", "set", f"{schema}.custom-keybinding:{path}"]
+    tmp = ["gsettings", "set", f"{schema}.custom-keybinding:{path}"]
     subprocess.call(tmp + ["name", wm_class])
     subprocess.call(tmp + ["command", f"focus-application {wm_class} {app}"])
     subprocess.call(tmp + ["binding", f"<Super>{key}"])
